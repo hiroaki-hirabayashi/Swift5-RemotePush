2a97798b6f43a60c09ddf44d34324aa6584f367c
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-06-20 13:51:04 +0900

    Initial Commit

---
+++ Swift5 RemotePush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5 RemotePush
+//
+//  Created by 平林宏淳 on 2020/06/20.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift5 RemotePushTests/Swift5_RemotePushTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift5_RemotePushTests.swift
+//  Swift5 RemotePushTests
+//
+//  Created by 平林宏淳 on 2020/06/20.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import XCTest
+@testable import Swift5_RemotePush
+
+class Swift5_RemotePushTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
